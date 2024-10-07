ab2276efaaaa8bb8cb5c1840d072bf76c12e6436
Alexander <stdAlexTikhonov@hotmail.com>
2024-10-07 11:01:06 +0300

    added basic table

--- apps/table/src/components/app/app.tsx
+++ apps/table/src/components/app/app.tsx
@@ -4,9 +4,21 @@ import styles from './app.module.scss';
 import { Footer } from '../footer';
 
 export function App() {
+
+  const arr = Array(10).fill(0);
   return (
     <div className={styles.root}>
-      <div className={styles.main} />
+      <div className={styles.main}>
+        <table className={styles.table}>
+          <tbody>
+            {arr.map((item, index) => <tr key={`row-${index}`}>
+              {
+                arr.map((_, i) => <td key={`col-${i}`}>1</td>)
+              }
+            </tr>)}
+          </tbody>
+        </table>
+      </div>
       <Footer />
     </div>
   );
