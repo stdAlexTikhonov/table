6fb890027a249549f6dab91f8008999fc84b2049
Alexander <stdAlexTikhonov@hotmail.com>
2024-10-27 02:37:39 +0300

    code refactor

--- apps/table/src/components/Input/Input.tsx
+++ apps/table/src/components/Input/Input.tsx
@@ -1,25 +1,8 @@
 import React from 'react';
-import { useAppDispatch, useAppSelector } from '../../hooks';
+import { useCommon } from '../../hooks';
 import styles from './Input.module.scss';
-import { commonSelectors } from '../../store/common/selectors';
-import { setValue } from '../../store/common/actions';
 
 export const Input = () => {
-  const dispatch = useAppDispatch();
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const val = e.target.value;
-    dispatch(setValue(val));
-  }
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.code === 'Tab') {
-      e.preventDefault();
-      e.stopPropagation();
-      alert('Tab pressed');
-    }
-  }
-
-  const value = useAppSelector(commonSelectors.value)
+  const { value, handleChange, handleKeyDown } = useCommon();
   return <input type="text" value={value} onChange={handleChange} onKeyDown={handleKeyDown} className={styles.root} />;
 }
--- apps/table/src/hooks/useCommon.ts
+++ apps/table/src/hooks/useCommon.ts
@@ -1,17 +1,33 @@
 import { Faker } from "@faker-js/faker/.";
 import { useAppDispatch, useAppSelector } from ".";
-import { setDataset } from "../store/common/actions";
+import { setDataset, setValue } from "../store/common/actions";
 import { commonSelectors } from "../store/common/selectors";
 
 export const useCommon = () => {
+  const dispatch = useAppDispatch();
+
   const actionType = useAppSelector(commonSelectors.actionType);
   const datasets = useAppSelector(commonSelectors.datasets);
   const dataset = useAppSelector(commonSelectors.dataset);
   const columns = useAppSelector(commonSelectors.columns);
   const length = useAppSelector(commonSelectors.length);
   const data = useAppSelector(commonSelectors.data);
+  const value = useAppSelector(commonSelectors.value)
 
-  const dispatch = useAppDispatch();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value;
+    dispatch(setValue(val));
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === 'Tab') {
+      e.preventDefault();
+      e.stopPropagation();
+      alert('Tab pressed');
+    }
+  }
+
+
 
   const handleSetDataset = (val: keyof Faker) => {
     dispatch(setDataset(val));
@@ -24,6 +40,9 @@ export const useCommon = () => {
     dataset,
     columns,
     length,
-    data
+    data,
+    value,
+    handleChange,
+    handleKeyDown
   }
 };
