661a6666a40d18fd117c9ec431f68f4a24333c4e
Alexander <stdAlexTikhonov@hotmail.com>
2024-12-05 23:07:52 +0300

    fix(TASK-4): added material

--- apps/table/src/main.tsx
+++ apps/table/src/main.tsx
@@ -5,6 +5,11 @@ import { Provider } from 'react-redux';
 import { App } from './components';
 import { store } from './store';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
